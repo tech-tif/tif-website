70abd5929118bb3e0e9f26bdc27ddbc7a8814dd1
lahi-ravindra-nalawade <ravindra.nalawade@lendahandindia.org>
2026-08-21 12:04:12 +0530

    Google Sheet Integration

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
    ============================================================ */
 window.TIF_CONFIG = {
   // Paste your Google Apps Script Web App URL here:
-  GAS_URL: "",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwkWLMUUl3XhotKOTSZqUzdAhfT7QTPuvpUCcf1HF3ddgG25LFpGOhiNUwX8Ea7QNYKuQ/exec",
   // Default language on first visit: "en", "mr", or "hi"
   DEFAULT_LANG: "en"
 };
